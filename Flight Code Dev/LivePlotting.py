99014a24819b9da4e19ffd56c6dc01c7ace124e0
JdogLloyd1 <jonylloyd123@gmail.com>
2023-01-31 15:36:25 -0600

    rearrange and start live plot

---
+++ Flight Code Dev/LivePlotting.py
@@ -0,0 +1,16 @@
+# This script will test live plotting in MatPlotLib as data is fed over time
+
+import numpy and np
+import matplotlib.pyplot as pyplot
+import time
+import sys
+import math
+import random 
+
+
+datalist = []
+index=1:1:10
+
+for i in index:
+	
+	datalist(i) = math.ceil(8*random.random())
