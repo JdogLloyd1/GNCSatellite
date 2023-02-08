d69a1f769ee2825e3f89fbf2f30244e758853d8e
JdogLloyd1 <jonylloyd123@gmail.com>
2023-02-08 16:16:00 -0600

    live IMU script works for any orientation, shows angular velocities

--- Flight Code Dev/IMU Tests/BNO055 Live Data.py
+++ Flight Code Dev/IMU Tests/BNO055 Live Data.py
@@ -5,6 +5,7 @@ import adafruit_bno055
 
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
+sensor.mode = adafruit_bno055.NDOF_MODE
 
 last_val = 0xFFFF
 print("Quaternion: {}".format(sensor.quaternion))
@@ -24,15 +25,32 @@ start_time = time.time()
 
 style.use('fivethirtyeight')
 
+#while sensor.calibrated != True:
+
+#	print("Sys/Gyro/Acc/Mag")
+#	print(sensor.calibration_status)
+#	print(sensor.calibrated)
+#	print("")
+#time.sleep(1)
+
 # Create figure for plotting
-fig = plt.figure()
-ax1 = fig.add_subplot(3, 1, 1)
-ax2 = fig.add_subplot(3, 1, 2)
-ax3 = fig.add_subplot(3, 1, 3)
+fig1 = plt.figure(1)
+ax1 = fig1.add_subplot(3, 1, 1)
+ax2 = fig1.add_subplot(3, 1, 2)
+ax3 = fig1.add_subplot(3, 1, 3)
+
+fig2 = plt.figure(2)
+ax4 = fig2.add_subplot(3, 1, 1)
+ax5 = fig2.add_subplot(3, 1, 2)
+ax6 = fig2.add_subplot(3, 1, 3)
+
 xs = []
 ys_roll = []
 ys_pitch = []
 ys_yaw = []
+ys_wx = []
+ys_wy = []
+ys_wz = []
 	
 print("Quaternion: {}".format(sensor.quaternion))
 
@@ -49,14 +67,20 @@ def animate_euler(i, xs, ys_roll, ys_pitch, ys_yaw): #function to plot euler ang
 	q3 = position[3]
 	
 	phi = math.atan2(2*(q0*q1+q2*q3), 1-2*(q1**2 + q2**2)) #roll rotation
-	theta = -math.pi/2 + 2*math.atan2(math.sqrt(1+2*(q0*q2 - q1*q3)), math.sqrt(1-2*q0*q2-q1*q3)) #pitch rotation
+	
+	#Have two options for pitch, use whichever works numerically
+	inside_fun = 2*(q0*q2 - q1*q3)
+	if abs(inside_fun) < 1:
+		theta = math.asin(2*(q0*q2 - q1*q3))
+	else:
+		theta = -math.pi/2 + 2*math.atan2(math.sqrt(1+2*(q0*q2 - q1*q3)), math.sqrt(1-2*q0*q2-q1*q3)) #pitch rotation
 	psi = math.atan2(2*(q0*q3+q1*q2), 1-2*(q2**2 + q3**2)) #yaw rotation
 
 	# Add x and y to lists
 	xs.append(time.time())
-	ys_roll.append(phi)
-	ys_pitch.append(theta)
-	ys_yaw.append(psi)
+	ys_roll.append(phi*180/math.pi)
+	ys_pitch.append(theta*180/math.pi)
+	ys_yaw.append(psi*180/math.pi)
 
 	# Limit x and y lists to 20 items
 	xs = xs[-20:]
@@ -72,8 +96,8 @@ def animate_euler(i, xs, ys_roll, ys_pitch, ys_yaw): #function to plot euler ang
 	#plt.xticks(rotation=45, ha='right')
 	#plt.subplots_adjust(bottom=0.30)
 	#plt.title('Roll')
-	plt.ylabel('roll')
-	leg = plt.legend()
+	#ax1.ylabel('roll')
+	leg1 = ax1.legend()
 
 	#plt.subplot(3,1,2)
 	ax2.clear()
@@ -81,8 +105,8 @@ def animate_euler(i, xs, ys_roll, ys_pitch, ys_yaw): #function to plot euler ang
 	#plt.xticks(rotation=45, ha='right')
 	#plt.subplots_adjust(bottom=0.50)
 	#plt.title('Pitch')
-	plt.ylabel('pitch')
-	leg = plt.legend()
+	#ax2.ylabel('pitch')
+	leg2 = ax2.legend()
 	
 	#plt.subplot(3,1,3)
 	ax3.clear()
@@ -90,12 +114,63 @@ def animate_euler(i, xs, ys_roll, ys_pitch, ys_yaw): #function to plot euler ang
 	#plt.xticks(rotation=45, ha='right')
 	#plt.subplots_adjust(bottom=0.50)
 	#plt.title('Yaw')
-	plt.ylabel('')
-	leg = plt.legend()
+	#ax3.ylabel('yaw')
+	leg3 = ax3.legend()
 	
+	print(sensor.calibrated)
 	
+def animate_omega(i, xs, ys_wx, ys_wy, ys_wz): #function to plot angular velocities in real time
+
+	#Read quaternion from IMU, translate to z-y-x euler angles
+	
+	position = sensor.quaternion
+	ang_vel = sensor.gyro
+	
+
+	# Add x and y to lists
+	#xs.append(time.time())
+	ys_wx.append(ang_vel[0]*180/math.pi)
+	ys_wy.append(ang_vel[1]*180/math.pi)
+	ys_wz.append(ang_vel[2]*180/math.pi)
+
+	# Limit x and y lists to 20 items
+	xs = xs[-20:]
+	ys_wx = ys_wx[-20:]
+	ys_wy = ys_wy[-20:]
+	ys_wz = ys_wz[-20:]
+
+	# Draw x and y lists
+
+	#plt.subplot(3,1,1)
+	ax4.clear()
+	ax4.plot(xs, ys_wx, label = 'omega x')
+	#plt.xticks(rotation=45, ha='right')
+	#plt.subplots_adjust(bottom=0.30)
+	#plt.title('Roll')
+	#ax1.ylabel('roll')
+	leg4 = ax4.legend()
+
+	#plt.subplot(3,1,2)
+	ax5.clear()
+	ax5.plot(xs, ys_wy, label = 'omega y')
+	#plt.xticks(rotation=45, ha='right')
+	#plt.subplots_adjust(bottom=0.50)
+	#plt.title('Pitch')
+	#ax2.ylabel('pitch')
+	leg5 = ax5.legend()
+	
+	#plt.subplot(3,1,3)
+	ax6.clear()
+	ax6.plot(xs, ys_wz, label = 'omega z')
+	#plt.xticks(rotation=45, ha='right')
+	#plt.subplots_adjust(bottom=0.50)
+	#plt.title('Yaw')
+	#ax3.ylabel('yaw')
+	leg6 = ax6.legend()
+	
+	#print(sensor.calibrated)	
 	
 # Set up plot to call animate() function periodically
-plt.figure(1)
-ani = animation.FuncAnimation(fig, animate_euler, fargs=(xs, ys_roll, ys_pitch, ys_yaw), interval=1000)
+ani1 = animation.FuncAnimation(fig1, animate_euler, fargs=(xs, ys_roll, ys_pitch, ys_yaw), interval=1000)
+ani2 = animation.FuncAnimation(fig2, animate_omega, fargs=(xs, ys_wx, ys_wy, ys_wz), interval=1000)
 plt.show()
