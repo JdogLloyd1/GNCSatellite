ad7d7efa5585b216fe53b0c3bea76b8df03b8d3f
Daniel Cohen <92395416+Ddandman@users.noreply.github.com>
2023-02-08 19:52:54 -0600

    Add files via upload

---
+++ Flight Code Dev/IMU Tests/attitude_control_functions.py
@@ -0,0 +1,94 @@
+import math
+
+def matrix_mult(matrix1, matrix2):
+
+    m = len(matrix1) #number of rows in matrix 1
+    K = len(matrix2[0])
+    res = [[0 for x in range(K)] for y in range(m)]
+     
+    # explicit for loops
+    for i in range(len(matrix1)):
+        for j in range(len(matrix2[0])):
+            for k in range(len(matrix2)):
+     
+                # resulted matrix
+                res[i][j] += matrix1[i][k] * matrix2[k][j]
+     
+    return res
+
+def make_omega_local(phi, theta, psi, omega):
+    #turns the IMU's angular velocity vector into local coordinates
+    #phi is roll, theta is pitch, psi is yaw, omega is gloabl angular velocity
+    
+    #general formula is to undo rotations: rotate -roll, then -pitch, then -yaw
+    rot_mat_x = [[1,0,0], [0, math.cos(-phi), -math.sin(-phi)], [0, math.sin(-phi), math.cos(-phi)]]
+    rot_mat_y = [[math.cos(-theta), 0, math.sin(-theta)], [0,1,0], [-math.sin(-theta), 0, math.cos(-theta)]]
+    rot_mat_z = [[math.cos(-psi), -math.sin(-psi), 0], [math.sin(-psi), math.cos(-psi), 0], [0, 0, 1]] 
+    rot_mat_xy = matrix_mult(rot_mat_x, rot_mat_y)
+    rot_mat_xyz = matrix_mult(rot_mat_xy, rot_mat_z)
+    omega_mat = matrix_mult(rot_mat_xyz, [[omega[0]], [omega[1]], [omega[2]]])
+    omega_local = [omega_mat[0][0], omega_mat[1][0], omega_mat[2][0]]
+    
+    return omega_local
+
+def maneuver_control(current_state, intended_state, col, calc_accl, vel_tar):
+    #col already has 1 subtracted so it's in 0 indexing. Will always be 0-2 for position
+    
+    torque = [0, 0, 0] #default value
+    
+    theta_dif = current_state[col] - intended_state[col]
+    omega_dif = current_state[col+3] - intended_state[col+3]
+    omega_cur = current_state[col + 3]
+    
+    if abs(theta_dif) < omega_cur**2/(2*calc_accl):
+        #if how long you have to go will take less then than slowing down, then slow down
+        
+        if omega_cur == 0:
+            omega_cur += .0001 #can't divide by zero
+        
+        torque[col] = -1*omega_cur/abs(omega_cur) #negative or positive 1 to slow it down
+        #change line below to elif
+    elif theta_dif > 0: #too far in positive direction and not close enough to slow down
+        if omega_cur >= -vel_tar: #unless moving fast enough in negative direction, move backwards
+            torque[col] = -1
+    
+    else: # too far rotated in negative direction and not close enough to slow down
+        if omega_cur <= vel_tar: #unless moving fast enough in positive direction, move forwards
+            torque[col] = 1
+    
+    return torque
+
+def attitude_control(current_state, intended_state, col, omega_tar):
+    #col is 0-5, can be position or rotation
+    
+    torque = [0, 0, 0] #default values
+    
+    if col < 3: #changing attitude position
+    
+        theta_dif = current_state[col] - intended_state[col]
+        omega_dif = current_state[col+3] - intended_state[col+3]
+        omega_cur = current_state[col + 3]
+        
+        if theta_dif > 0: #rotated too far in positive direction
+            if omega_cur >= -omega_tar: #unless I'm moving fast enough in negative direction, rotate backwards
+                torque[col] = -1
+        else: #too far rotated in negative direction
+            if omega_cur <= omega_tar: #unless I'm moving fast enough in positive direction, rotate forwards
+                torque[col] = 1
+                
+    else: #changing angular rate
+    
+        theta_dif = current_state[col-3] - intended_state[col-3]
+        omega_dif = current_state[col] - intended_state[col]
+        
+        if omega_dif > 0: #moving too fast in positive direction, slow down
+            torque[col - 3] = -1 
+        
+        elif omega_dif < 0: #moving too fast in negative direction, accelerate in positive direction
+            torque[col - 3] = 1
+        
+    return torque
+                
+            
+        
+    
