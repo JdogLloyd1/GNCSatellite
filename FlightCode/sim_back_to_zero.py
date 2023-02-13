320bc7e20e583b37bca9c3736722800eadefd3a4
Daniel Cohen <92395416+Ddandman@users.noreply.github.com>
2023-02-13 16:49:41 -0600

    Add files via upload

---
+++ FlightCode/sim_back_to_zero.py
@@ -0,0 +1,192 @@
+import math
+import time as t
+import matplotlib.pyplot as plt
+from attitude_control_functions import *
+
+## Next few lines are stuff for setup that gets completely
+# overwritten when we move away from the sim
+
+m_sc = 3.4958
+#current_state = [0]*6 #replace with IMU data
+state_mat = [] #for plotting, but IMU algorithm might need to track this so
+# might actually be necessary
+time = 0 #need to do something regarding time but prob different from this
+dt = .01
+time_vec = []
+roll_vec = []
+pitch_vec = []
+yaw_vec = []
+yw_vec = []
+wx_vec = []
+wy_vec = []
+wz_vec = []
+## Next few lines are stuff for setup that will stay the same
+
+end_con = 0 #binary variable that becomes 1 when final event is complete
+current_event = 0  #add 1 for every new maneuver, starts at 0 bc that's the first index
+event_complete = 0 #binary variable that = 0 when in maneuver and 1 when
+#in between maneuvers
+event_ending_time = [] #add time where each event ends
+
+I_inv = [[1.4425, -.1697, -2.5*10**-4], [-.1697, 1.4425, 2.4*10**-4], \
+         [-2.5*10**-4, -2.4*10**-4, 1.273]] #use this to estimate slow down time. UPDATE
+    
+#build the event matrix - format in documentation. Variables you're changing are 1-6, 
+#first 3 are angular positions then angular velocities. Note they'r 1 above python indeces
+event_mat =  [[0,.25,.1,3, math.pi/2,.01], [0,.25,.1,2,-math.pi/3,.05], [0,.25,.1,2,0,.05], [0,.25,.1,3,math.pi,.05] ]
+              # do maneuvers below to get back to 0 at end, keep simple for now
+              #0 .25 .1 8 0 .05; 0 .25 .1 9 pi/2 .
+
+current_state = [30*math.pi/180, 45*math.pi/180, 20*math.pi/180, 15*math.pi/180, 25*math.pi/180, 30*math.pi/180]
+intended_state = [0]*6 
+change_var = event_mat[0][3] - 1 #the variable we're changing, -1 is so indeces are right
+#intended_state[change_var] = event_mat[0][4]
+
+num_attitude_checks = 1
+            
+state_check = [3, 4, 5, 0, 3, 1, 4, 2, 5]#, 0, 3, 1, 4, 2, 5] 
+#order of attitude checks you do, putting in vector to allow for easy changing
+#Fix the x, then the z, then the y.0 is x position, 3 is x velocity
+
+thruster_mat = [0, 0] #track which thrusters turn on, might not need this         
+ 
+
+## Long while loop is the heart of the control algorithm for control logic, won't change    
+x = t.time()  
+        
+while num_attitude_checks < len(state_check):
+    
+    #case 3 of 4 is you've finished the event and slowed down but NOT all attitudes are fixed
+
+    
+    torque = [0, 0, 0] #default values
+    
+    theta_tol = .2*math.pi/180  #tolerance for position, number is in degrees
+    omega_tol = .1*math.pi/180
+
+    if num_attitude_checks < 10:
+        omega_tar = 10*math.pi/180 #target rotational velocity during attitude position fixes
+    else:
+        omega_tar = 2*math.pi/180
+    current_col = state_check[num_attitude_checks - 1]  
+    
+    if current_col < 3: #fixing an attitude position
+        
+        if abs(current_state[current_col] - intended_state[current_col]) > theta_tol:
+            #Attitude is outside of tolerance, fix it
+            
+            torque = attitude_control(current_state, intended_state, current_col, omega_tar)
+        
+        else: #atttitude is good, move onto next one
+        
+            num_attitude_checks += 1
+            
+    elif current_col < 6: #fixing angular rate
+    
+        if abs(current_state[current_col] - intended_state[current_col]) > omega_tol:
+            #Attitude is outside of tolerance, fix it
+            
+            torque = attitude_control(current_state, intended_state, current_col, omega_tar)
+        
+        else: #atttitude is good, move onto next one
+        
+            num_attitude_checks += 1            
+            
+
+
+
+                
+    ## End of event logic. Lines of code below won't change
+    
+    #figure out which thrusters to turn on based on torque -doesn't change
+    #numerical integration stuff
+    
+    if torque[0] > 0: #positive roll
+        thruster_pair = [2, 4] #1y, 2y
+    elif torque[0] < 0: #negative roll
+        thruster_pair = [6, 8] #3y, 4y
+    elif torque[1] > 1: #positive pitch
+        thruster_pair = [1, 7] #1x, 4x
+    elif torque[1] < 1: #negative pitch
+        thruster_pair = [3, 5] #2x, 3x
+    elif torque[2] > 1: #positive yaw
+        thruster_pair = [3, 7] #2x, 4x
+    elif torque[2] < 1: #negative yaw
+        thruster_pair = [1, 5] #1x, 3x
+    else: #no thrust
+        thruster_pair = [0, 0]
+    
+        
+       
+    
+    ##numerical integration steps that I won't need once we move away from that
+    time += dt
+    
+    alpha = current_state[2] #yaw
+    beta = current_state[1] #pitch
+    gamma = current_state[0] #roll
+    
+    rot_mat_x = [[1,0,0], [0, math.cos(gamma), -math.sin(gamma)], [0, math.sin(gamma), math.cos(gamma)]]
+    rot_mat_y = [[math.cos(beta), 0, math.sin(beta)], [0,1,0], [-math.sin(beta), 0, math.cos(beta)]]
+    rot_mat_z = [[math.cos(alpha), -math.sin(alpha), 0], [math.sin(alpha), math.cos(alpha), 0], [0, 0, 1]] 
+    rot_mat_yx = matrix_mult(rot_mat_y, rot_mat_x)
+    
+    #numerical integration of acceleration to velocity
+    
+    ang_accl = matrix_mult([torque], I_inv) #have to make the torque vector a matrix
+    current_state[3] += dt*ang_accl[0][0]*.15 #scale by the magnitude
+    current_state[4] += dt*ang_accl[0][1]*.15
+    current_state[5] += dt*ang_accl[0][2]*.15
+    
+    #numerical integration of velocity to position
+    gamma += dt*current_state[3]
+    
+    y_delta = matrix_mult(rot_mat_yx, [[0], [current_state[4]],[0] ])
+    gamma += dt*y_delta[0][0] #scale by timestep
+    beta += dt*y_delta[1][0]
+    alpha += dt*y_delta[2][0]
+    
+    z_delta = matrix_mult(rot_mat_yx, [[0], [0], [current_state[5]]])
+    gamma += dt*z_delta[0][0] #scale by timestep
+    beta += dt*z_delta[1][0]
+    alpha += dt*z_delta[2][0]
+    
+    current_state[2] = alpha
+    current_state[1] = beta
+    current_state[0] = gamma
+    
+    roll_vec.append(current_state[0]*180/math.pi)
+    pitch_vec.append(current_state[1]*180/math.pi)
+    yaw_vec.append(current_state[2]*180/math.pi)
+    wx_vec.append(current_state[3]*180/math.pi)
+    wy_vec.append(current_state[4]*180/math.pi)
+    wz_vec.append(current_state[5]*180/math.pi)
+    time_vec.append(time)
+     
+plt.figure(1)
+#first figure is for angular positions
+
+plt.subplot(321)
+plt.plot(time_vec, roll_vec, label = 'Roll')
+leg = plt.legend()
+plt.subplot(323)
+plt.plot(time_vec, pitch_vec, label = 'Pitch')
+leg = plt.legend()
+plt.subplot(325)
+plt.plot(time_vec, yaw_vec, label = 'Yaw')
+leg = plt.legend()
+
+#plt.figure(2)
+#second figure is for angular velocities
+
+plt.subplot(322)
+plt.plot(time_vec, wx_vec, label = 'Omega x')
+leg = plt.legend()
+plt.subplot(324)
+plt.plot(time_vec, wy_vec, label = 'Omega y')
+leg = plt.legend()
+plt.subplot(326)
+plt.plot(time_vec, wz_vec, label = 'Omega z')
+leg = plt.legend()
+
+    
